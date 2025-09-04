d8e93707d55f66205981b02c94fb6204f2c0910a
Jaden M <jrmantie@hotmail.com>
2025-09-03 21:50:13 -0600

    added proper information to projects page, fixed spacing issues for titles and descriptions

--- e-portfolio/src/components/Project.jsx
+++ e-portfolio/src/components/Project.jsx
@@ -4,8 +4,10 @@ import "../css/Project.css";
 function Project({project}) {
     return <div className="project">
         <img className="prj-image" src={project.img} alt={project.title}/>
-        <h3 className="racing-sans-one-regular">Title: {project.title}</h3>
-        <p className="roboto">Description: {project.description}</p>
+        <section className="prj-text">
+            <h3 className="racing-sans-one-regular prj-text"><i>Title:</i> {project.title}</h3>
+            <p className="roboto prj-text"><i>Description:</i> {project.description}</p>
+        </section>
     </div>
 }
 
--- e-portfolio/src/pages/Projects.jsx
+++ e-portfolio/src/pages/Projects.jsx
@@ -1,13 +1,36 @@
 
 import Project from "../components/Project.jsx"
+import GameHub from "../assets/GameHub-project.png"
+import Construction from "../assets/construction.jpg"
+
+const projList = [
+    {
+    title: "Game Hub",
+    img: GameHub,
+    description: "A game hub for two educational games"
+    },
+    {
+        title: "Grant Management System",
+        img: Construction,
+        description: "This project is currently under development. Will be added once there is a project to show."
+    },
+    {
+        title: "Unknown",
+        img: Construction,
+        description: "Lots more coming. Come back later to see more..."
+    },
+    {
+        title: "Unknown",
+        img: Construction,
+        description: "Lots more coming. Come back later to see more..."
+    }
+]
 
 function Projects() {
     return <div className="base">
-        <Project project={ {
-            title: "First Project",
-            img: "https://thewowstyle.com/wp-content/uploads/2015/01/nature-images..jpg",
-            description: "This is a sample project"
-        }} />
+        {projList.map((prj, ind) => {
+            return <Project project={prj} key={ind}/>
+    })}
     </div>
 }
 
